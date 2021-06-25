4ec8c511b94581412dfd9b4369b0bf7e8924776e
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-06-25 16:36:17 +0200

    Added babel-loader to webpack config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-const App = () => {
-	return null
-}
+import React from 'react'
+
+// Webpack can only deal with plain JS and not JSX
+// A "loader" like Babel is needed so JSX gets processed before being bundled by webpack
+
+const App = () => <div>hello webpack</div>
 
 export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,31 @@
 const path = require('path')
 
 // __dirname is a global variable from Node that has the current directory name
+
+//////// LOADERS
+// Loaders are defined in the module property and it's rules array
+// To define a loader it consists out of 3 parts
+// 1. Test specifies the loaders is for .js files
+// 2. Loader prop specifies the loader which will be used
+// 3. Options is used to configure the loaders functionality
+////////
+
 const config = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'build'),
 		filename: 'main.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-react'],
+				},
+			},
+		],
 	},
 }
 
