48c330363b384e25281b315100ae93833b6d0c12
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-06-25 16:43:51 +0200

    Installed babel-polyfill

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react'
 
-// Webpack can only deal with plain JS and not JSX
-// A "loader" like Babel is needed so JSX gets processed before being bundled by webpack
-
-const App = () => <div>hello webpack</div>
+const App = () => <div>hello world</div>
 
 export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path')
 
 // __dirname is a global variable from Node that has the current directory name
+// babel-polyfill is needed so async/await can be used on all browsers
+// polyfill means replicating the use of an API in browsers that do not support it
 
 //////// LOADERS
+// Webpack can only deal with plain JS and not JSX
+// A "loader" like Babel is needed so JSX gets processed before being bundled by webpack
 // Loaders are defined in the module property and it's rules array
 // To define a loader it consists out of 3 parts
 // 1. Test specifies the loaders is for .js files
@@ -11,7 +15,7 @@ const path = require('path')
 ////////
 
 const config = {
-	entry: './src/index.js',
+	entry: ['@babel/polyfill', './src/index.js'],
 	output: {
 		path: path.resolve(__dirname, 'build'),
 		filename: 'main.js',
