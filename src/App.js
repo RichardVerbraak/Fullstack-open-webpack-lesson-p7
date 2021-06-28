f79793d6697d593a88ef7ccd082f37491736cae0
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-06-28 15:14:02 +0200

    Added a sidenote comment about how state is calculated from within a function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
 import './index.css'
 
+// Additional info on the comment below: https://stackoverflow.com/questions/54069253/usestate-set-method-not-reflecting-change-immediately
+
 const App = () => {
 	const [counter, setCounter] = useState(0)
-	const [value, setValue] = useState()
+	const [value, setValue] = useState([])
 
+	// side note: the state values used in this function are based on the current closure
+	// the state will be updated after re-render and the values in this functions closure will not be affected
+	// simply said, state in this function will stay true to whatever the value was in that point in time
 	const handleClick = () => {
 		setCounter(counter + 1)
-		setValue(counter)
+		setValue([...value, counter])
 	}
 
 	return (
