31563dfaa433706f13e603e02a9c9cc4f10ebf92
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-06-28 15:18:34 +0200

    Installed axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import './index.css'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 // Additional info on the comment below: https://stackoverflow.com/questions/54069253/usestate-set-method-not-reflecting-change-immediately
 
