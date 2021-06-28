a88a57e19c4aff45ee3bf93b8b6a970e3f5d0301
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-06-28 15:26:27 +0200

    Changed config to be a function that returns an object instead of just a object

--- webpack.config.js
+++ webpack.config.js
@@ -27,33 +27,35 @@ const path = require('path')
 // The devServer will bundle the files BUT it will only exist in memory and won't be output in a file like main.js
 // The changes will only be reflected on localhost port 3000
 
-const config = {
-	entry: ['@babel/polyfill', './src/index.js'],
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: 'main.js',
-	},
-	devServer: {
-		contentBase: path.resolve(__dirname, 'build'),
-		compress: true,
-		port: 3000,
-	},
-	devtool: 'source-map',
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				options: {
-					presets: ['@babel/preset-env', '@babel/preset-react'],
+const config = (env, arg) => {
+	return {
+		entry: ['@babel/polyfill', './src/index.js'],
+		output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: 'main.js',
+		},
+		devServer: {
+			contentBase: path.resolve(__dirname, 'build'),
+			compress: true,
+			port: 3000,
+		},
+		devtool: 'source-map',
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+					},
 				},
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader'],
+				},
+			],
+		},
+	}
 }
 
 module.exports = config
